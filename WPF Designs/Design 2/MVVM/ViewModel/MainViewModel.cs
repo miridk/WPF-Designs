aff48d16215a57da1f7dfb0aabba7e5314a32d8e
miridk <Rinkovski@live.dk>
2022-02-11 23:26:16 +0100

    Done the most of the design from the source video

---
+++ WPF Designs/Design 2/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Design_2.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_2.MVVM
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
