9b5c7508d8325dc9e1e6079b9a6f686924485506
Michael Rinkovski <rinkovski@live.dk>
2022-02-12 20:11:42 +0100

    Finnished the Design2 resourse

--- WPF Designs/Design 2/MVVM/ViewModel/MainViewModel.cs
+++ WPF Designs/Design 2/MVVM/ViewModel/MainViewModel.cs
@@ -5,12 +5,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Design_2.MVVM
+namespace Design_2.MVVM.ViewModel
 {
     internal class MainViewModel : ObservableObject
     {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand MicroserviceViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+        public MicroserviceViewModel MicroserviceVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
         public MainViewModel()
         {
+            HomeVM = new HomeViewModel();
+            MicroserviceVM = new MicroserviceViewModel();
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            MicroserviceViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = MicroserviceVM;
+            });
 
         }
     }
