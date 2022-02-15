cd95710d553b8c078273bc8afba7eaaee817cbe7
Michael Rinkovski <rinkovski@live.dk>
2022-02-15 10:35:16 +0100

    Design four with flow is created. tention is needed on the next button and other buttons

---
+++ WPF Designs/Design 4/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Design_4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void homeBtn_Clicked(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new HomeView();
+        }
+
+        private void microserviceBtn_Clicked(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new MicroservicesView();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void closBtn(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
